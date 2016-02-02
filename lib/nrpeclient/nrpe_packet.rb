51ba1cce0ed695eea28f79318e8f8ef03e8b93a2
Dmitriy Martynov <dmitriy.martynov@proquest.com>
2016-02-02 14:33:12 -0500

    This change adds a bit of functionality to the basic NRPE packet libary to parse performance data from the buffer into a separate hash under packet.perfdata. It does not change any other functionality in any way - i.e. the packet.buffer will still return the same thing it always did - entire contents of the buffer, including status and performance data. One could argue that this can be done at a higher level, and perhaps it is, though I haven't found a good enough definition of the NRPE protocol to say one way or another. This change just makes it a bit more convenient to do perf data things.

--- lib/nrpeclient/nrpe_packet.rb
+++ lib/nrpeclient/nrpe_packet.rb
@@ -13,7 +13,7 @@ module Nrpeclient
 
     MAX_PACKET_SIZE = 12 + 1024
 
-    attr_accessor :packet_version, :crc32, :result_code, :buffer
+    attr_accessor :packet_version, :crc32, :result_code, :buffer, :perfdata
 
     def initialize(unpacked=nil)
       @packet_version = NRPE_PACKET_VERSION_2
@@ -26,6 +26,15 @@ module Nrpeclient
         @result_code    = unpacked[3]
         @buffer         = unpacked[4]
         @random         = unpacked[5]
+        # Parse perfdata and return as a hash
+        if @buffer.include? "|" then
+          @perfdata = Hash.new
+          @buffer.split("|")[1].split(",").each do |metric|
+            key = metric.split("=")[0].strip
+            value = metric.split("=")[1].strip
+            @perfdata[:"#{key}"] = value
+          end
+        end
       end
     end
 
